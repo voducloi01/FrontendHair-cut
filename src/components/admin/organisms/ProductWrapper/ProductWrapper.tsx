d8a7e83901dcb0b2f66ef6513d70e11290d849f4
loivd2001 <loivd@vitalify.asia>
2023-08-28 14:44:17 +0700

    fix conflict

--- src/components/admin/organisms/ProductWrapper/ProductWrapper.tsx
+++ src/components/admin/organisms/ProductWrapper/ProductWrapper.tsx
@@ -1,5 +1,9 @@
 import TableComponent from '@/components/molecules/TableComponent/TableComponent';
 import { ColumnProduct, DataProductType } from '@/type';
+import { Button } from '@mui/material';
+import { useState } from 'react';
+import DialogCreateProduct from '../dialogProduct';
+import { useTranslation } from 'react-i18next';
 
 //TODO
 const columns: ColumnProduct[] = [
@@ -27,9 +31,39 @@ interface ProductWrapperProps {
 
 //TODO
 const ProductWrapper = ({ product }: ProductWrapperProps) => {
+  const [dialog, setDialog] = useState<boolean>(false);
+  const { t } = useTranslation();
+
+  const handleCreateProduct = () => {
+    setDialog(true);
+  };
+
+  const handleCancelDialog = () => {
+    setDialog(false);
+  };
+
+  const handleSubmit = () => {
+    console.log(123);
+  };
+
   return (
     <div>
       <h1 className="text_title">Manager Product</h1>
+      <div>
+        <Button
+          color="success"
+          size="medium"
+          variant="contained"
+          onClick={handleCreateProduct}
+        >
+          {t('product.create')}
+        </Button>
+      </div>
+      <DialogCreateProduct
+        isDialog={dialog}
+        handleCancel={handleCancelDialog}
+        handleSubmit={handleSubmit}
+      />
       <TableComponent />
     </div>
   );
