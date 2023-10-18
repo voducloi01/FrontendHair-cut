906715eee7415ef72b0f1a9b3e1a909f55ada4d4
loivd2001 <loivd@vitalify.asia>
2023-10-18 12:35:11 +0700

    add range price

--- src/components/organisms/IntroduceWrapper/IntroduceWrapper.tsx
+++ src/components/organisms/IntroduceWrapper/IntroduceWrapper.tsx
@@ -18,9 +18,17 @@ interface IntroduceWrapper {
 
 const IntroduceWrapper = ({ dataProduct, category }: IntroduceWrapper) => {
   const [dataCategory, setDataCategory] = useState<string>('0');
+  const [valueRange, setValueRange] = useState<number>(0);
+
+  /// Price maximum
+  const maxPrice = Math.max(...dataProduct.map((product) => product.price));
 
   const ChangeCategory = (e: SelectChangeEvent) => {
     setDataCategory(e.target.value);
+  };
+
+  const handleChangeRange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValueRange(Number(e.target.value));
   };
 
   return (
@@ -45,12 +53,22 @@ const IntroduceWrapper = ({ dataProduct, category }: IntroduceWrapper) => {
             })}
           </Select>
         </FormControl>
+        <input
+          className="mt-4"
+          type="range"
+          min="1"
+          max={maxPrice}
+          value={valueRange || maxPrice}
+          onChange={handleChangeRange}
+        />
+        <div className="fs-20 ml-1">{formatPrice(valueRange || maxPrice)}</div>
       </div>
       <div className="wrapper__introduce__grid">
         {dataCategory !== '0'
           ? category
               .find((e) => e.id === dataCategory)
-              ?.products.map((v) => {
+              ?.products.filter((c) => !valueRange ? maxPrice : valueRange >= c.price)
+              .map((v) => {
                 return (
                   <div key={v.id} className="wrapper__introduce__grid__item">
                     <img
@@ -62,18 +80,20 @@ const IntroduceWrapper = ({ dataProduct, category }: IntroduceWrapper) => {
                   </div>
                 );
               })
-          : dataProduct.map((e) => {
-              return (
-                <div key={e.id} className="wrapper__introduce__grid__item">
-                  <img
-                    className="wrapper__introduce__grid__item__img"
-                    src={e.urlImg}
-                  />
-                  <div>{e.productName}</div>
-                  <div>{formatPrice(e.price)}</div>
-                </div>
-              );
-            })}
+          : dataProduct
+              .filter((v) => !valueRange ? maxPrice : valueRange >= v.price)
+              .map((e) => {
+                return (
+                  <div key={e.id} className="wrapper__introduce__grid__item">
+                    <img
+                      className="wrapper__introduce__grid__item__img"
+                      src={e.urlImg}
+                    />
+                    <div>{e.productName}</div>
+                    <div>{formatPrice(e.price)}</div>
+                  </div>
+                );
+              })}
       </div>
     </div>
   );
