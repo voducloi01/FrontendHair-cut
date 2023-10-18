f5af6ef7206b24703b65ed5a881b192be7887734
loivd2001 <loivd@vitalify.asia>
2023-10-18 11:00:31 +0700

    refactor traniing

--- src/pages/Training.tsx
+++ src/pages/Training.tsx
@@ -2,12 +2,14 @@ import { Container } from '@mui/material';
 import TrainingWrapper from '@/components/organisms/TrainingWrapper/TrainingWrapper';
 import Header from '@/components/atoms/Header/Header';
 import { DATA_INFORMATION } from '@/constants/constant';
+import Footer from '@/components/atoms/Footer/Footer';
 
 const Training = () => {
   return (
     <Container id="training">
       <Header />
       <TrainingWrapper dataTraining={DATA_INFORMATION} />
+      <Footer />
     </Container>
   );
 };
